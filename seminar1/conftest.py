fecc342f2c4a8b43431947f5988d51f1cf88624e
MZukhra <109167494+MZukhra@users.noreply.github.com>
2023-08-22 10:28:26 +0300

    Update conftest.py

--- seminar1/conftest.py
+++ seminar1/conftest.py
@@ -12,12 +12,14 @@ password = adress['password']
 
 @pytest.fixture()
 def get_token():
+    """Авторизация и получение токена"""
     res = requests.post(url=adress["url1"], data={"username": username, "password": password})
     token = res.json()["token"]
     return token
 
 @pytest.fixture()
 def get_description(get_token):
+    """Создание нового поста и получение описания"""
     res = requests.post(url=adress["url2"], headers={"X-Auth-Token": get_token}, data={"username": username, "password": password, "title": adress["title"],
                                                   "description": adress["description"], "content": adress["content"]})
     return res.json()["description"]
