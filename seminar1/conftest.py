6dfc90d327a93cebf238df56dfe9ae76ccb15dfd
MZukhra <109167494+MZukhra@users.noreply.github.com>
2023-08-22 10:25:20 +0300

    Add files via upload

---
+++ seminar1/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+import requests
+
+import yaml
+
+with open("config2.yaml") as f:
+    adress = yaml.safe_load(f)
+
+username = adress['username']
+password = adress['password']
+
+
+@pytest.fixture()
+def get_token():
+    res = requests.post(url=adress["url1"], data={"username": username, "password": password})
+    token = res.json()["token"]
+    return token
+
+@pytest.fixture()
+def get_description(get_token):
+    res = requests.post(url=adress["url2"], headers={"X-Auth-Token": get_token}, data={"username": username, "password": password, "title": adress["title"],
+                                                  "description": adress["description"], "content": adress["content"]})
+    return res.json()["description"]
+
+
+
