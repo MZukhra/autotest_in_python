96f7ea1a79fbc3c450dcd41253eebde37e2c1b24
MZukhra <109167494+MZukhra@users.noreply.github.com>
2023-08-22 10:31:26 +0300

    Update conftest.py

--- seminar1/conftest.py
+++ seminar1/conftest.py
@@ -4,24 +4,24 @@ import requests
 import yaml
 
 with open("config2.yaml") as f:
-    adress = yaml.safe_load(f)
+    info = yaml.safe_load(f)
 
-username = adress['username']
-password = adress['password']
+username = info['username']
+password = info['password']
 
 
 @pytest.fixture()
 def get_token():
     """Авторизация и получение токена"""
-    res = requests.post(url=adress["url1"], data={"username": username, "password": password})
+    res = requests.post(url=info["url1"], data={"username": username, "password": password})
     token = res.json()["token"]
     return token
 
 @pytest.fixture()
 def get_description(get_token):
     """Создание нового поста и получение описания"""
-    res = requests.post(url=adress["url2"], headers={"X-Auth-Token": get_token}, data={"username": username, "password": password, "title": adress["title"],
-                                                  "description": adress["description"], "content": adress["content"]})
+    res = requests.post(url=info["url2"], headers={"X-Auth-Token": get_token}, data={"username": username, "password": password, "title": info["title"],
+                                                  "description": info["description"], "content": info["content"]})
     return res.json()["description"]
 
 
